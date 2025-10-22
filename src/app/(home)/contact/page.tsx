b183f824a96d3b4415bd5972a52d614c48d02ae2
Elvis Tehinse <elvis.omotehinse@neukleos.com>
2025-10-22 08:27:48 +0100

    add base

--- src/app/(home)/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContactUs from "@/contact/ContactUs";
-
-export default function Contactpage() {
-  return (
-    <main className="w-full">
-        <ContactUs />
-    </main>
-  );
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
 
-import Hero from "@/components/Hero";
-import Intro from "@/components/Intro";
-import About from "@/components/About";
-import WhatWeDo from "@/components/WhatWeDo";
-import WhyWeDoIt from "@/components/WhyWeDoIt";
-import OurInitiatives from "@/components/OurInitiatives";
-import Discover from "@/components/Discover";
-import VideoSection from "@/components/VideoSection";
-import Navbar from "@/components/Navbar";
-import OurPartner from "@/components/OurPartner";
-import PartnerWithUs from "@/components/PartnerWithUs";
-import Footer from "@/components/Footer";
 
 export default function Home () {
   return (
     <main>
-      <Navbar />
-      <Hero />
-      <Intro />
-      <About />
-      <WhatWeDo />
-      <WhyWeDoIt />
-      <OurInitiatives />
-      <Discover />
-      <VideoSection />
-      <OurPartner />
-      <PartnerWithUs />
-      <Footer />
+Welcome
     </main>
   );
 }
